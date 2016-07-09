5008198f6311ed6e360103a34536ad04e804ab78
Antonio Neto <antoniomrneto@gmail.com>
2016-07-09 14:43:33 -0300

    2

--- fp/exerc/2/mini_calculadora.ex
+++ fp/exerc/2/mini_calculadora.ex
@@ -1,25 +1,28 @@
 defmodule MiniCalculadora do
-  @doc """
-  Dado uma tupla de entrada do tipo: {atom, valor1, valor1}. No qual:
-  atom = :soma || :multiplica || :subtrai || :divide
-  valor1 = número
-  valor2 = número
+	@doc """
+	Dado uma tupla de entrada do tipo: {atom, valor1, valor2}. No qual:
+	atom = :soma || :multiplica || :subtrai || :divide
+	valor1 = número
+	valor2 = número
 
-  Crie uma estrutura que realize a operação usando qualquer aplicação  do
-  PatternMatching
+	Crie uma estrutura que realize a operação usando qualquer aplicação  do
+	PatternMatching
 
-  Exemplo:
-    entrada: {:soma, 1, 1}
-    saída: 2
+	Exemplo:
+	entrada: {:soma, 1, 1}
+	saída: 2
 
-    entrada: {:multiplica, 2, 2}
-    saída: 4
+	entrada: {:multiplica, 2, 2}
+	saída: 4
 
-    entrada: {:to_list, 33, 10}
-    saída: 23
+	entrada: {:to_list, 33, 10}
+	saída: 23
 
-    entrada: {nil, 1, 7}
-    saída: 'invalid'
-  """
-
+	entrada: {nil, 1, 7}
+	saída: 'invalid'
+	"""
+	def calc({atom, valor1, valor2}) when atom == :soma, do: valor1 + valor2
+	def calc({atom, valor1, valor2}) when atom == :multiplica, do: valor1 * valor2
+	def calc({atom, valor1, valor2}) when atom == :subtrai, do: valor1 - valor2
+	def calc({atom, valor1, valor2}) when atom == :divide, do: valor1 / valor2
 end
