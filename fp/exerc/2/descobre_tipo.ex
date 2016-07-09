77b6cde8253ea3cf3d84a03fee9b0ea9001d7a67
Antonio Neto <antoniomrneto@gmail.com>
2016-07-09 14:54:46 -0300

    2

--- fp/exerc/2/descobre_tipo.ex
+++ fp/exerc/2/descobre_tipo.ex
@@ -1,16 +1,18 @@
 defmodule DescobreTipo do
-  @moduledoc """
-  Crie uma função que recebe um valor e descobre se é integer, float ou não é
-  um número.
+	@moduledoc """
+	Crie uma função que recebe um valor e descobre se é integer, float ou não é
+	um número.
 
-  Exemplo:
-    entrada: 1
-    saída: é integer
+	Exemplo:
+	entrada: 1
+	saída: é integer
 
-    entrada: "abc"
-    saída: não é um número
+	entrada: "abc"
+	saída: não é um número
 
-  Esse problema deve ser resolvido usando Guards
-  """
-
+	Esse problema deve ser resolvido usando Guards
+	"""
+	def is_num(n) when is_integer(n), do: "é integer"
+	def is_num(n) when is_float(n), do: "é flloat"
+	def is_num(n), do: "não é um número"
 end
