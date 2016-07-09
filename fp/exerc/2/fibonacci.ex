c0c80f4194cd924b47e3dc9638f8c5265eb90e08
Antonio Neto <antoniomrneto@gmail.com>
2016-07-09 14:48:47 -0300

    2

--- fp/exerc/2/fibonacci.ex
+++ fp/exerc/2/fibonacci.ex
@@ -1,13 +1,20 @@
 defmodule Fibonacci do
-  @moduledoc """
-  Construir um algoritmo para imprimir a série de FIBONACCI. A série de FIBONACCI
-  é formada pela sequência:
+	@moduledoc """
+	Construir um algoritmo para imprimir a série de FIBONACCI. A série de FIBONACCI
+	é formada pela sequência:
 
-  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55...
+	0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55...
 
-  Essa sequência tem uma lei de formação simples: cada elemento, a partir do
-  terceiro, é obtido somando-se os dois anteriores.
-  Veja: 1+1=2, 2+1=3, 3+2=5, 3+5=8, 5+8=13...
-  """
-
+	Essa sequência tem uma lei de formação simples: cada elemento, a partir do
+	terceiro, é obtido somando-se os dois anteriores.
+	Veja: 1+1=2, 2+1=3, 3+2=5, 3+5=8, 5+8=13...
+	"""
+	def fib(0), do: 1
+	def fib(1), do: 1
+	def fib(n) do
+		fib(n-1) + fib(n-2)
+	end
+	def print_fib(n) do
+		fib(n) |> IO.puts
+	end 
 end
