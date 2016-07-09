a68d140081677d8427985ba3fe859fdf67926205
Leonardo <leo.rog@gmail.com>
2016-07-08 21:45:22 -0300

    fix

--- FP/exerc/extra/decrypt/server.exs
+++ FP/exerc/extra/decrypt/server.exs
@@ -1,14 +1,15 @@
 defmodule Server do
-
+  require PasswordBucket
   def start do
     password_bucket_pid = PasswordBucket.start
-    pid = spawn(__MODULE__, :loop, [password_bucket_pid, 0])
+    spawn(__MODULE__, :loop, [password_bucket_pid, 0])
   end
 
   def loop(pid, current_password_position) do
     receive do
       {:request, worker_pid} ->
-        PasswordBucket.get_next(pid, current_password_position)
+        next_password = PasswordBucket.get_next(pid, current_password_position)
+        send(worker_pid, {:response, next_password})
         loop(pid, current_password_position+1)
       {:found, password, position} ->
         PasswordBucket.mark_as_checked(pid, position)
