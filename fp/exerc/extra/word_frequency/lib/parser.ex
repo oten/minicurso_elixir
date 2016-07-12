42aed1e847fec3545389e8100deb40067f5bd664
Antonio Neto <antoniomrneto@gmail.com>
2016-07-11 23:21:39 -0300

    extra

--- fp/exerc/extra/word_frequency/lib/parser.ex
+++ fp/exerc/extra/word_frequency/lib/parser.ex
@@ -1,3 +1,27 @@
 defmodule WordFreq.Parser do
-
+	def to_arr(file_name) do
+		{:ok, texto} = File.read file_name
+		arr = texto
+			  |> String.Casing.downcase 
+			  |> String.split 
+	end
+	
+	def add_freq(termo, m) do
+		if m[termo] == nil do
+			m = Map.put(m, termo, 1)
+		else
+			m = Map.put(m, termo, Map.get(m, termo) + 1)
+		end
+		m
+	end
+	
+	def group(arr, m) when arr == [], do: m
+	def group(arr, m) do
+		m = arr 
+			|> hd 
+			|> add_freq(m)
+		arr 
+		|> tl 
+		|> group(m)
+	end
 end
